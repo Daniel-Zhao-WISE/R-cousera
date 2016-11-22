3ab78c337a3c860d07eafedf85024453c2b80a58
Daniel Zhao <danielzhaowise@gmail.com>
2016-11-22 15:56:55 +0800

    Update pollutantmean.R
    Add the comment of this function

--- W2/pollutantmean.R
+++ W2/pollutantmean.R
@@ -1,4 +1,12 @@
+## This function return the mean of the pollution value of a pollutant for some range of ids
+## Data are in the specdata.rar, and all is .csv file
+
 pollutantmean <- function(directory, pollutant, id = 1:332) {
+        ## variable description
+        ## directory is the directory of the files
+        ## pollutant can be "sulfate" or "nitrate"
+        ## id is the id number of the monitor, and it's also the name of each file in the specdata.rar
+        
         pollu2 <- c()
         for (item in id) {
                 if(item < 10){
